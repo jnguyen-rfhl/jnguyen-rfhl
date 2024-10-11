983e8127cdeee98c868995bbe94659d91cb8fc26
Jam Nguyen <jam.nguyen@rfhl.com>
2024-10-11 19:44:02 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 import 'simplebar-react/dist/simplebar.min.css';
 
-import { AuthRouter, OverlayScroller } from './components';
-import { AuthenticatedPage, UnauthenticatedPage } from './pages/root';
+import { OverlayScroller } from './components';
+import { GoogleApiKeyCheckerPage } from './pages/google-api-key-checker';
 import { store } from './redux';
 import { defaultTheme } from './styles';
 
@@ -32,8 +32,7 @@ export function App() {
 function RootApp() {
   return (
     <Box p={2}>
-      <AuthRouter protectedPage={<AuthenticatedPage />} publicPage={<UnauthenticatedPage />} />
-      {/* Global components */}
+      <GoogleApiKeyCheckerPage />
     </Box>
   );
 }
--- src/pages/root/Unauthenticated.tsx
+++ src/pages/root/Unauthenticated.tsx
@@ -1,22 +1,5 @@
-import { Navigate, RouteObject, useRoutes } from 'react-router-dom';
-
-import { ROUTE } from '@/constants';
-
 import { GoogleApiKeyCheckerPage } from '../google-api-key-checker';
 
-const unauthorizedRoutes: RouteObject[] = [
-  {
-    path: ROUTE.GOOGLE_API_KEY_CHECKER,
-    element: <GoogleApiKeyCheckerPage />,
-  },
-  {
-    path: '*',
-    element: <Navigate to={ROUTE.HOME} />,
-  },
-];
-
 export function UnauthenticatedPage() {
-  const unauthElements = useRoutes(unauthorizedRoutes);
-
-  return unauthElements;
+  return <GoogleApiKeyCheckerPage />;
 }
