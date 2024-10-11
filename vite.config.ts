5fdb79500cb0087703a8be4cf74cb3958b4648ac
Jam Nguyen <jam.nguyen@rfhl.com>
2024-10-11 19:33:22 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import svgr from 'vite-plugin-svgr';
 import path from 'node:path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    svgr(),
-  ],
+  plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     port: 3000,
   },
